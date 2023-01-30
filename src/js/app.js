8fdeb1e58781f617ad4ba9298deee1d439297341
Andrew Koltsov <excitenew@gmail.com>
2023-01-30 20:47:03 +0200

    Minor changes to .hmtl and style files. Implement popup with a11y support.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+import {
+	disableBodyScroll,
+	enableBodyScroll,
+	clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+import A11yDialog from 'a11y-dialog';
 import * as slider from './modules/slider.js';
 import * as func from './modules/functions.js';
 
@@ -23,56 +29,22 @@ if (menuToggle) {
 }
 
 function toggleBurgerInteractionClasses() {
-	// disable scroll on the page while menu is open
-	document.body.classList.toggle('scroll-lock');
+	if (!header.classList.contains('header--scrolled')) {
+		header.classList.add('header--scrolled');
+	}
 	menuToggle.classList.toggle('menu-button--active');
 	menuBody.classList.toggle('menu__body--active');
 }
 
-// const pcMenuContainer = document.getElementById('nav-other');
-// const mobileContacts = document.querySelector('.menu-mobile__contacts');
-// const menuItemsList = document.getElementById('other-dropdown');
-
-// const smallDevice = window.matchMedia('(min-width: 48em)');
-
-// smallDevice.addListener(handleDeviceChange);
-
-// function handleDeviceChange(e) {
-// 	if (e.matches) {
-// 		pcMenuContainer.append(menuItemsList);
-// 	} else {
-// 		mobileContacts.before(menuItemsList);
-// 	}
-// }
-
-// // Run it initially
-// handleDeviceChange(smallDevice);
-
 /// watching page scroll to remove extra padding from header
 /// after scrolling a bit from the top of the page
 const header = document.querySelector('.header');
-const mainSectionAnchor = document.querySelector('.wrapper');
-const heroContainer = document.querySelector('.hero__container');
-// const mainSectionAnchor = document.querySelector('.hero');
-const headerSectionOptions = {
-	// root: mainSectionAnchor,
-	// rootMargin: '-10px 0px 0px 0px',
-	// rootMargin: `${header.offsetHeight * -1}px`,
-	rootMargin: `${header.offsetHeight * 1}px`,
-	threshold: 1,
-	// threshold: 0,
-};
 
 var checkHeader = _.throttle(() => {
 	let scrollPosition = Math.round(window.scrollY);
-	// If we've scrolled 100px, add "sticky" class to the header
 	if (scrollPosition > 0) {
-		// document.querySelector('header').classList.add('sticky');
 		header.classList.add('header--scrolled');
-	}
-	// If not, remove "sticky" class from header
-	else {
-		// document.querySelector('header').classList.remove('sticky');
+	} else {
 		header.classList.remove('header--scrolled');
 	}
 	// Run JavaScript stuff here
@@ -80,61 +52,10 @@ var checkHeader = _.throttle(() => {
 
 window.addEventListener('scroll', checkHeader);
 
-console.log(header.offsetHeight * 1);
-console.log(headerSectionOptions.rootMargin);
+const popupContainer = document.getElementById('call-back-popup');
+const dialog = new A11yDialog(popupContainer);
+const body = document.querySelector('body');
 
-// const headerSectionObserver = new IntersectionObserver(function (entries) {
-// 	entries.forEach((entry) => {
-// 		// if (entry.isIntersecting) {
-// 		// console.log(mainSectionAnchor.scrollTop);
-// 		console.log(entry.intersectionRatio);
-// 		console.log(entry.isIntersecting);
-// 		// if (entry.intersectionRatio < 1) {
-// 		// if (mainSectionAnchor.scrollTop > 0) {
-// 		if (entry.isIntersecting) {
-// 			// header.classList.add('header--scrolled');
-// 			header.classList.add('header--scrolled');
-// 		} else {
-// 			header.classList.remove('header--scrolled');
-// 			// header.classList.remove('header--scrolled');
-// 		}
-// 	});
-// }, headerSectionOptions);
-
-// headerSectionObserver.observe(heroContainer);
-
-// /// listening to intersectioning with site section
-// /// to add underline to site navigation link
-// const navMenuLinks = document.querySelectorAll('.menu__link');
-// const navMenuObserver = new IntersectionObserver(
-// 	function (entries) {
-// 		entries.forEach((entry) => {
-// 			// if (entry.isIntersecting && entries.intersectionRatio > 0.5) {
-// 			if (entry.isIntersecting) {
-// 				navMenuLinks.forEach((link) => {
-// 					const targetSection = link.getAttribute('href').replace('#', '');
-// 					link.classList.toggle(
-// 						'menu__link--active',
-// 						targetSection === entry.target.id
-// 					);
-// 				});
-// 			}
-// 		});
-// 	},
-// 	{
-// 		threshold: 0.2,
-// 	}
-// );
-
-// document
-// 	.querySelectorAll('.section')
-// 	.forEach((section) => navMenuObserver.observe(section));
-
-// /// scroll to site section and clost mobile after the click on link
-// navMenuLinks.forEach((link) => {
-// 	link.addEventListener('click', function (event) {
-// 		if (menuBody.classList.contains('menu__body--active')) {
-// 			toggleBurgerInteractionClasses();
-// 		}
-// 	});
-// });
+dialog
+	.on('show', () => disableBodyScroll(body))
+	.on('hide', () => enableBodyScroll(body));
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -4,41 +4,24 @@ import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 const mainSlider = new Swiper('.hero__slider', {
-	// slidesPerView: 'auto',
-	// spaceBetween: 20,
-	// slidesPerView: 1,
-	// centeredSlides: true,
-	// autoplay: {
-	// 	delay: 2500,
-	// 	disableOnInteraction: false,
-	// },
-	// loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	loop: true,
 	effect: 'fade',
 	pagination: {
 		el: '.hero__slider-pagination',
 	},
 	fadeEffect: { crossFade: true },
-	breakpoints: {
-		900: {
-			// slidesPerView: 3,
-			// spaceBetween: 30,
-		},
-		1100: {
-			// spaceBetween: 30,
-		},
-	},
 });
 
 const teamSlider = new Swiper('.team__slider', {
-	// slidesPerView: 'auto',
-	// spaceBetween: 20,
-	// slidesPerView: 1,
-	// centeredSlides: true,
-	// autoplay: {
-	// 	delay: 2500,
-	// 	disableOnInteraction: false,
-	// },
-	// loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	loop: true,
 	effect: 'fade',
 	pagination: {
 		el: '.team__slider-pagination',
@@ -48,13 +31,4 @@ const teamSlider = new Swiper('.team__slider', {
 		prevEl: '.team__slider-prev-btn',
 	},
 	fadeEffect: { crossFade: true },
-	breakpoints: {
-		900: {
-			// slidesPerView: 3,
-			// spaceBetween: 30,
-		},
-		1100: {
-			// spaceBetween: 30,
-		},
-	},
 });
